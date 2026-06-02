3956933a45bf71289239c0848058b55d2b8fec84
Aline Exposto <aline.exposto@escola.pr.gov.br>
2026-06-02 11:39:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function mostrarMensagem() {
+    alert(
+        "A agricultura inteligente utiliza drones, sensores e inteligência artificial para produzir mais alimentos e proteger o meio ambiente."
+    );
+}
 
+function mostrarCuriosidade() {
+
+    const curiosidade = document.getElementById("curiosidade");
+
+    curiosidade.innerHTML =
+        "🌱 Curiosidade: Sensores modernos conseguem detectar a necessidade de irrigação antes mesmo que a planta apresente sinais visíveis de falta de água.";
+}
